af8c34a3a65e8f9f881317dc3a91d5d3ed6a42af
c230175 <c230175@hsakuragaoka-h.ed.jp>
2024-11-27 02:55:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
+
+app.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs');
+
+let person = {
+  name:"俵山航",
+  nickname: "わたる",  
+  birthday:"２月２７日" ,        
+  zodiac: "うお座", 
+ 
+};
+
+console.log("ニックネーム: " + person.nickname);
+console.log("星座: " + person.zodiac);
+console.log("誕生日: " + person.birthday);
+
+
+for (let key in person) {
+  console.log(key + ": " + person[key]);
+}
+
+
+
+
+app.get('/api', (req, res) => {
+  res.json({"msg": "Hello world"});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+})
